5eebfc1954c628b738661a6f45ff856407c7f6d8
xMichaelRodriguez <scottlovos503@gmail.com>
2022-04-16 16:13:13 -0600

    paginas lazyload

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,26 +2,33 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  NavLink
+  NavLink,
 } from 'react-router-dom';
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
 
 import logo from '../logo.svg';
 
 export const Navigation = () => {
   return (
     <Router>
-      <div className="main-layout">
+      <div className='main-layout'>
         <nav>
-            <img src={ logo } alt="React Logo" />
+          <img src={logo} alt='React Logo' />
           <ul>
             <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
+              <NavLink to='/lazy1' activeClassName='nav-active' exact>
+                Lazily Loaded Component 1
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
+              <NavLink to='/lazy2' activeClassName='nav-active' exact>
+                Lazily Loaded Component 2
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
+              <NavLink to='/lazy3' activeClassName='nav-active' exact>
+                Lazily Loaded Component 3
+              </NavLink>
             </li>
           </ul>
         </nav>
@@ -29,17 +36,11 @@ export const Navigation = () => {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <h1>About</h1>
-          </Route>
-          <Route path="/users">
-            <h1>Users</h1>
-          </Route>
-          <Route path="/">
-            <h1>Home</h1>
-          </Route>
+          <Route path='/lazy1' component={LazyPage1} />
+          <Route path='/lazy2' component={LazyPage2} />
+          <Route path='/lazy3' component={LazyPage3} />
         </Switch>
       </div>
     </Router>
   );
-}
+};
